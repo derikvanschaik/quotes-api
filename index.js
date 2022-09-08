ffde66797783e200e0790a73dfbcc4b50b5ef1ec
Derik van Schaik <derikvanschaik@gmail.com>
2022-09-08 14:38:01 -0700

    add cors

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const port = process.env.PORT || 3000
 
 const app = createApp(quotesService, imageService);
 
+// allow cors 
+app.use((req, res, next) => {
+  res.header({"Access-Control-Allow-Origin": "*"});
+  next();
+}) 
+
 app.listen(port, () => {
   if(port === 3000){
     console.log('App running at: http://localhost:3000');
